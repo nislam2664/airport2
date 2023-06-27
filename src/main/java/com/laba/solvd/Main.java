59b59f99bcfd10a72fd5d4bfeaa4f85e107a06b6
nislam2664 <taranee567@gmail.com>
2023-06-27 11:32:14 -0400

    updated main

--- src/main/java/com/laba/solvd/Main.java
+++ src/main/java/com/laba/solvd/Main.java
@@ -1,8 +1,15 @@
 package com.laba.solvd;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.laba.solvd.model.Airplane;
+import com.laba.solvd.parsers.JSONParser;
+import com.laba.solvd.parsers.Parser;
 import com.laba.solvd.parsers.StAXParser;
 import com.laba.solvd.validator.Validator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,13 +17,17 @@ public class Main {
         String jsonPath = "src/main/resources/airport.json";
         String xsdPath = "src/main/resources/airport.xsd";
 
+        Parser parser = null;
+
         System.out.println("*** XML VALIDATOR ***");
         Validator.validateXMLWithXSD(xmlPath, xsdPath);
 
         System.out.println("\n*** STAX PARSER ***");
-        StAXParser staxParser = new StAXParser();
-        Airplane staxAirplane = staxParser.parse(xmlPath);
+        parser = new StAXParser();
+        Airplane staxAirplane = parser.parse(xmlPath);
 
-
+        System.out.println("\n*** JSON PARSER ***");
+        parser = new JSONParser();
+        Airplane jsonAirplane = parser.parse(jsonPath);
     }
 }
